52ca38d9c32bb6675ce9ccd43c2e8bb73a3acca4
IShnitko <ilyshabogenika@gmail.com>
2024-05-11 21:52:47 +0200

    Commit test 2

--- frame/GameFrame.java
+++ frame/GameFrame.java
@@ -3,10 +3,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameFrame extends JFrame {
-    JButton confirm;
+    JPanel sidePanel;
     JCheckBox side;
+    JButton sideButton;
+    ImageIcon trueCheck = new ImageIcon("frame/true-check.png");
+    ImageIcon falseCheck = new ImageIcon("frame/false-check.png");
+    JPanel troopsNumberPanel;
+    JLabel troopsNumberLabel;
+    JButton troopsNumberButton;
     JTextField troopsNumber;
-    JRadioButton troopsType;
+    ButtonGroup troopsType;
+    JPanel troopsTypePanel;
     JRadioButton archer;
     JRadioButton swordsman;
     JRadioButton shieldman;
@@ -16,19 +23,46 @@ public class GameFrame extends JFrame {
         this.setLayout(new BorderLayout(3, 3));
 
         menu = new JPanel();
-        menu.setLayout(new GridLayout(4,2));
+        menu.setLayout(new GridLayout(2,2));
 
-        troopsType = new JRadioButton();
+        troopsType = new ButtonGroup();
+        troopsTypePanel = new JPanel();
+        troopsTypePanel.setLayout(new GridLayout(3, 1));
+
         archer = new JRadioButton("Archer");
         swordsman = new JRadioButton("Swordsman");
         shieldman = new JRadioButton("Shieldman");
         troopsType.add(archer);
         troopsType.add(swordsman);
         troopsType.add(shieldman);
-        troopsType.setLayout(new FlowLayout());
+        troopsTypePanel.add(archer);
+        troopsTypePanel.add(swordsman);
+        troopsTypePanel.add(shieldman);
 
-        menu.add(troopsType);
-        menu.setBackground(Color.BLACK);
+        troopsNumberPanel = new JPanel();
+        troopsNumberLabel = new JLabel("Enter amount of troops: ");
+        troopsNumberButton = new JButton("Submit number");
+        troopsNumberPanel.setLayout(new GridLayout(3,1));
+        troopsNumber = new JTextField();
+        troopsNumberPanel.add(troopsNumberLabel);
+        troopsNumberPanel.add(troopsNumber);
+        troopsNumberPanel.add(troopsNumberButton);
+
+        sidePanel = new JPanel();
+        sidePanel.setLayout(new GridLayout(2,1));
+        sideButton = new JButton("Submit");
+        side = new JCheckBox("Side");
+        side.setFocusable(false);
+        side.setIcon(falseCheck);
+        side.setSelectedIcon(trueCheck);
+        side.setBackground(Color.BLACK);
+        sidePanel.add(side);
+        sidePanel.add(sideButton);
+        System.out.println(side.isSelected());
+
+        menu.add(sidePanel);
+        menu.add(troopsNumberPanel);
+        menu.add(troopsTypePanel);
 
         this.add(menu, BorderLayout.LINE_START);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
